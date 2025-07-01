1469878d4f6fe3fc488e83a55cb4c02ab32280a0
andre-szp <andre.szp@outlook.com>
2025-06-30 22:48:22 -0300

    chore: add @ToString annotation to model classes

--- src/main/java/com/CadastroDeMusicos/Orquestra/Instrumentos/InstrumentoModel.java
+++ src/main/java/com/CadastroDeMusicos/Orquestra/Instrumentos/InstrumentoModel.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @Table(name = "tb_instrumentos")
 public class InstrumentoModel {
     @Id
